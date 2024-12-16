2b48c1b5d312e9ec02b2e80e4991174ea0135caa
Vidar Magnusson <git@vidarmagnusson.com>
2024-12-16 10:45:49 +0100

    Solve day 16

--- day16/src/solution.rs
+++ day16/src/solution.rs
@@ -1,6 +1,6 @@
 use std::{
     cmp::Reverse,
-    collections::{BinaryHeap, HashMap},
+    collections::{BinaryHeap, HashMap, HashSet},
     usize,
 };
 
@@ -25,6 +25,15 @@ impl Direction {
             Direction::East => (pos_x + 1, pos_y),
             Direction::South => (pos_x, pos_y + 1),
             Direction::West => (pos_x - 1, pos_y),
+        }
+    }
+
+    fn move_from(&self, pos_x: usize, pos_y: usize) -> (usize, usize) {
+        match self {
+            Direction::North => (pos_x, pos_y + 1),
+            Direction::East => (pos_x - 1, pos_y),
+            Direction::South => (pos_x, pos_y - 1),
+            Direction::West => (pos_x + 1, pos_y),
         }
     }
 
@@ -99,8 +108,6 @@ fn find_cheapest_path(input: Map) -> u64 {
     queue.push(Reverse((0, start, Direction::East)));
 
     while let Some(Reverse((cost, (x, y), dir))) = queue.pop() {
-        println!("Checkign {x},{y} for cost {cost} and dir {dir:?}");
-
         tile_cost.insert((x, y, dir.clone()), cost);
 
         if (x, y) == end {
@@ -122,7 +129,7 @@ fn find_cheapest_path(input: Map) -> u64 {
             let prev_cost = tile_cost
                 .get(&(nx, ny, rot_dir.clone()))
                 .unwrap_or(&u64::MAX);
-            let new_cost = cost + 1000;
+            let new_cost = cost + 1001;
             if &new_cost < prev_cost {
                 queue.push(Reverse((new_cost, (nx, ny), rot_dir.clone())));
             }
@@ -134,13 +141,12 @@ fn find_cheapest_path(input: Map) -> u64 {
             let prev_cost = tile_cost
                 .get(&(nx, ny, rot_dir.clone()))
                 .unwrap_or(&u64::MAX);
-            let new_cost = cost + 1000;
+            let new_cost = cost + 1001;
             if &new_cost < prev_cost {
                 queue.push(Reverse((new_cost, (nx, ny), rot_dir.clone())));
             }
         }
     }
-
     panic!("No path found!");
 }
 
@@ -148,6 +154,129 @@ pub fn solve_part_one<'a>(input: Map) -> String {
     find_cheapest_path(input).to_string()
 }
 
+fn find_tiles(input: Map) -> u64 {
+    let Map { layout, start, end } = input;
+
+    let mut tile_cost: HashMap<(usize, usize, Direction), u64> = HashMap::new();
+    let mut queue = BinaryHeap::new();
+    queue.push(Reverse((0, start, Direction::East)));
+
+    let mut best_path_cost = u64::MAX;
+
+    while let Some(Reverse((cost, (x, y), dir))) = queue.pop() {
+        tile_cost.insert((x, y, dir.clone()), cost);
+
+        if cost > best_path_cost {
+            continue;
+        }
+
+        if (x, y) == end {
+            best_path_cost = best_path_cost.min(cost);
+        }
+
+        let (nx, ny) = dir.move_in(x, y);
+        if layout[ny][nx] == Tile::Empty {
+            let prev_cost = tile_cost.get(&(nx, ny, dir.clone())).unwrap_or(&u64::MAX);
+            let new_cost = cost + 1;
+            if &new_cost < prev_cost {
+                queue.push(Reverse((new_cost, (nx, ny), dir.clone())));
+            }
+        }
+
+        let rot_dir = dir.rotate_clockwise();
+        let (nx, ny) = rot_dir.move_in(x, y);
+        if layout[ny][nx] == Tile::Empty {
+            let prev_cost = tile_cost
+                .get(&(nx, ny, rot_dir.clone()))
+                .unwrap_or(&u64::MAX);
+            let new_cost = cost + 1000;
+            if &new_cost < prev_cost {
+                queue.push(Reverse((new_cost, (x, y), rot_dir.clone())));
+            }
+        }
+
+        let rot_dir = dir.rotate_counter_clockwise();
+        let (nx, ny) = rot_dir.move_in(x, y);
+        if layout[ny][nx] == Tile::Empty {
+            let prev_cost = tile_cost
+                .get(&(nx, ny, rot_dir.clone()))
+                .unwrap_or(&u64::MAX);
+            let new_cost = cost + 1000;
+            if &new_cost < prev_cost {
+                queue.push(Reverse((new_cost, (x, y), rot_dir.clone())));
+            }
+        }
+    }
+
+    let mut to_check = [
+        Direction::North,
+        Direction::East,
+        Direction::South,
+        Direction::West,
+    ]
+    .into_iter()
+    .filter_map(|d| tile_cost.get(&(end.0, end.1, d.clone())).map(|c| (d, *c)))
+    .filter(|(_, c)| c == &best_path_cost)
+    .map(|(d, _)| ((best_path_cost, end, d)))
+    .collect::<Vec<_>>();
+
+    let mut visited = HashSet::new();
+
+    while let Some((cost, pos, dir)) = to_check.pop() {
+        visited.insert(pos);
+
+        if cost == 0 {
+            continue;
+        }
+
+        let prev = dir.move_from(pos.0, pos.1);
+        if let Some(&prev_cost) = tile_cost.get(&(prev.0, prev.1, dir.clone())) {
+            if prev_cost == cost - 1 {
+                to_check.push((prev_cost, prev, dir.clone()));
+            }
+        }
+
+        if cost >= 1000 {
+            let rot_dir = dir.rotate_clockwise();
+            let prev = rot_dir.move_from(pos.0, pos.1);
+
+            if let Some(&prev_cost) = tile_cost.get(&(prev.0, prev.1, rot_dir.clone())) {
+                if prev_cost == cost - 1001 {
+                    to_check.push((prev_cost, prev, rot_dir.clone()));
+                }
+            }
+
+            let rot_dir = dir.rotate_counter_clockwise();
+            let prev = rot_dir.move_from(pos.0, pos.1);
+
+            if let Some(&prev_cost) = tile_cost.get(&(prev.0, prev.1, rot_dir.clone())) {
+                if prev_cost == cost - 1001 {
+                    to_check.push((prev_cost, prev, rot_dir.clone()));
+                }
+            }
+        }
+    }
+
+    // // Print map
+    // for (y, row) in layout.iter().enumerate() {
+    //     for (x, t) in row.iter().enumerate() {
+    //         match t {
+    //             Tile::Wall => print!("#"),
+    //             Tile::Empty => {
+    //                 if visited.contains(&(x, y)) {
+    //                     print!("O");
+    //                 } else {
+    //                     print!(".");
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     print!("\n");
+    // }
+
+    visited.len() as u64
+}
+
 pub fn solve_part_two<'a>(input: Map) -> String {
-    todo!("Part two is not yet implemented");
+    find_tiles(input).to_string()
 }
